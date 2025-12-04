2097446f301be9f7ae2e23d2d70405d4abc2c820
nitramnauj <141964349+nitramnauj@users.noreply.github.com>
2025-12-04 08:55:32 -0600

    Angles in Samples
    This code calculates all possible angles between all all cRCC in a sample. To use:
    python3 c4_angleNhistoCalculator.py
    A request will appear, that must be answered with, by example: d9NoLat,Synthetic. That is to say, with the conditions to generate parallels and if the samples are random real or synthetic.
    
    To graph the histograms, just run graphHistogram.py

---
+++ c4_angleNhistoCalculator.py
@@ -0,0 +1,76 @@
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import csv
+
+def calcular_angulo(v1, v2):
+	"""Calcula el ángulo en grados entre dos vectores usando producto punto."""
+	dot = np.dot(v1, v2)
+	norm_v1 = np.linalg.norm(v1)
+	norm_v2 = np.linalg.norm(v2)
+	cos_theta = dot / (norm_v1 * norm_v2)
+	cos_theta = np.clip(cos_theta, -1.0, 1.0)  # evitar errores numéricos
+	return np.degrees(np.arccos(cos_theta))
+
+# Cargar archivo CSV
+file_name = input('Input samples ("d9NoLat,Synthetic"):')
+
+
+name = file_name.split(',')[0]
+#kind = 'Random'
+kind = file_name.split(',')[1]
+samples = [1,2500]
+
+for s in range(samples[0],samples[1]+1):
+	if kind == 'Random':
+		df = pd.read_csv('Samples_'+kind+'/size'+name+'N0/sample_'+name+'N0_'+str(s)+'.csv')
+	else:
+		df = pd.read_csv('Samples_'+kind+'/size'+name+'N0/synthetic_'+name+'N0_'+str(s)+'.txt')
+
+	# Detectar número de columnas
+	num_cols = df.shape[1]
+
+	# Extraer etiquetas de la primera columna
+	etiquetas = df.iloc[:, 0].values
+
+	# Seleccionar las 26 columnas intermedias
+	data = df.iloc[:, 1:27].values
+
+	# Crear bins dinámicamente
+	bin_size = 2.5
+	max_value = 90
+	bins = [(i,i+bin_size) for i in range(0,int(max_value),int(bin_size))]
+	bins[-1] = (bins[-1][0],max_value) # Aseguramos que el último bin incluya a 90°
+	counts = defaultdict(int)
+
+	# Archivo de salida
+	with open("Samples_"+kind+"/size"+name+"N0/pars_"+name+"_"+str(s)+".csv", mode="w", newline="") as f:
+		writer = csv.writer(f)
+		writer.writerow(["etiqueta_A", "etiqueta_B", "angulo_grados"])
+
+		# Calcular todos los pares posibles sin repetición
+		n = len(data)
+		for i in range(n):
+			for j in range(i+1, n):  # asegura que no se repita A con B y B con A
+				angulo = calcular_angulo(data[i], data[j])
+				# imprimir conforme se calcula
+				#print(f"{etiquetas[i]} vs {etiquetas[j]}: {angulo:.2f}°")
+				# guardar en archivo de paralelos
+				if angulo < 5:
+					writer.writerow([etiquetas[i], etiquetas[j], f"{angulo:.2f}"])
+				else:
+					pass
+				if 0 <= angulo <= max_value:
+					# Buscamos el índice de bin para colocarlo
+					bin_index = int(angulo // bin_size)
+					# Caso especial: max_value va en el último bin
+					if angulo == max_value:
+						bin_index = len(bins) - 1
+					counts[bin_index] += 1/(samples[1]-samples[0]+1)
+	print('Sample',s,'is ready')
+
+
+histogram = open('Samples_'+kind+'/histogram_'+kind[0].lower()+kind[1:4]+'_'+name+'_'+str(samples[0])+'_'+str(samples[1])+'.csv','w')
+for i, (low, high) in enumerate(bins):
+	print(f"{high},{counts[i]}",file=histogram)
+histogram.close()
---
+++ graphHistogram.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+distancias = [5,6,7,8,9]
+distancias = [6]
+laterales = ['','No']
+
+muestras = ['full','rand','synt']
+
+for d in distancias:
+    for s in laterales:
+        for m in muestras:
+            eje_x = []
+            eje_y = []
+            if m == 'rand' and s == 'No':
+                archivo = open('histogram_'+m+'_d'+str(d)+s+'Lat_1_2500.csv')
+            else:
+                archivo = open('histogram_'+m+'_d'+str(d)+s+'Lat.csv')
+            for line in archivo:
+                aux = list(map(float,line[:-1].split(',')))
+                eje_x.append(aux[0])
+                eje_y.append(aux[1])
+            archivo.close()
+            eje_x = np.array(eje_x)
+            eje_y = np.array(eje_y)
+            eje_y = 100*eje_y/sum(eje_y)
+
+            if s == '':
+                style = 'solid'
+            else:
+                style = 'dashed'
+
+            if m == 'full':
+                col = 'orange'
+            elif m == 'rand':
+                col = 'green'
+            else:
+                col = 'red'
+            
+            plt.plot(eje_x,eje_y,linestyle=style,color=col)
+    plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+    plt.title(f'Angle distribution at {d} $\AA$')
+    plt.xlabel('Angle between any 2 cRCC (°)')
+    plt.ylabel('Frequency (%)')
+    #plt.savefig(f'angleDistribution_d{d}.png')
+    plt.show()
+    plt.clf()
